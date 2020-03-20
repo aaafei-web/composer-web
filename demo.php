bce2c72d3663663778fe4f8568fe65bb8b8072e8
阿飞 <1625895247@qq.com>
2020-03-20 20:15:17 +0800

    我的第一个版本

---
+++ demo.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'vendor/autoload.php';
+use Abstarfish\Starfish\Tools;
+Tools::Hello();
---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,11 @@
+<?php
+namespace Abstarfish\Starfish;
+class Tools{
+	public function __countruct(){
+
+	}
+
+	public static function Hello(){
+		echo 'Hello word';
+	}
+}
